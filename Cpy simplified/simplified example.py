5cab29db671d2fa149d739a7117e93d204e396cd
WIZnet HK Ron <ronpang@wiznet.hk>
2022-05-04 16:21:04 +0800

    Update simplified example.py

--- Cpy simplified/simplified example.py
+++ Cpy simplified/simplified example.py
@@ -1,26 +1,25 @@
 import board
 import digitalio
 import time
-import gc
 from wiznet_simplify import *
 
 print("Wiznet5k SimpleServer Test (DHCP)")
+# init and create a TCP network
 TCP_server = network()
+# Create a socket for TCP server
 TCP_server.connection()
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
-# Maintain DHCP lease (continue the DHCP setting while TCP is connected)
-    #eth.maintain_dhcp_lease()
-
     led.value = not led.value #showing the light is blinking
     time.sleep(0.1) #transmit data speed
     
+    # Check TCP mode from the socket and starts the connection
     TCP_server.check_mode()
-    #TCP_mode.test()
+    # Starts communicate with other device
     received = TCP_server.communication()
     #print (received)
-    print( gc.mem_free() )
+
           
